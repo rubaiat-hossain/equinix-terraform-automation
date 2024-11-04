8d6e3bc1649fdae1481f32532d23418af442ac98
Rubaiat Hossain <rubaiatcse14@gmail.com>
2024-11-04 20:00:01 +0600

    added networking

--- main.tf
+++ main.tf
@@ -12,20 +12,6 @@ resource "equinix_metal_vlan" "vlan2" {
   project_id  = var.project_id
 }
 
-# Create a Metal Gateway for VLAN in Silicon Valley
-resource "equinix_metal_gateway" "gateway_sv" {
-  project_id               = var.project_id
-  vlan_id                  = equinix_metal_vlan.vlan1.id
-  private_ipv4_subnet_size = 16   # Adjust subnet size as needed
-}
-
-# Create a Metal Gateway for VLAN in New York
-resource "equinix_metal_gateway" "gateway_ny" {
-  project_id               = var.project_id
-  vlan_id                  = equinix_metal_vlan.vlan2.id
-  private_ipv4_subnet_size = 16   # Adjust subnet size as needed
-}
-
 # Deploy node1 in Silicon Valley
 resource "equinix_metal_device" "node1" {
   hostname         = "node1"
@@ -44,45 +30,41 @@ resource "equinix_metal_device" "node2" {
   operating_system = "ubuntu_20_04"
 }
 
-# Set Silicon Valley device's bond0 port to Layer 2 mode
-resource "equinix_metal_port" "bond0_sv" {
-  port_id = element([for p in equinix_metal_device.node1.ports: p.id if p.name == "bond0"], 0)
-  bonded  = true
-  layer2  = true
+# Set Hybrid network type for Layer 3 public IP, avoid bonding/unbonding configuration
+resource "equinix_metal_device_network_type" "node1_network" {
+  device_id = equinix_metal_device.node1.id
+  type      = "hybrid"
 }
 
-# Set New York device's bond0 port to Layer 2 mode
-resource "equinix_metal_port" "bond0_ny" {
-  port_id = element([for p in equinix_metal_device.node2.ports: p.id if p.name == "bond0"], 0)
-  bonded  = true
-  layer2  = true
+resource "equinix_metal_device_network_type" "node2_network" {
+  device_id = equinix_metal_device.node2.id
+  type      = "hybrid"
 }
 
-# Attach Silicon Valley device's bond0 to VLAN
+# Attach VLANs to `eth1` without setting the bonding flag directly
 resource "equinix_metal_port_vlan_attachment" "vlan_attachment_sv" {
   device_id = equinix_metal_device.node1.id
-  port_name = "bond0"
+  port_name = "eth1"
   vlan_vnid = equinix_metal_vlan.vlan1.vxlan
-  depends_on = [equinix_metal_port.bond0_sv]
+  depends_on = [equinix_metal_device_network_type.node1_network]
 }
 
-# Attach New York device's bond0 to VLAN
 resource "equinix_metal_port_vlan_attachment" "vlan_attachment_ny" {
   device_id = equinix_metal_device.node2.id
-  port_name = "bond0"
+  port_name = "eth1"
   vlan_vnid = equinix_metal_vlan.vlan2.vxlan
-  depends_on = [equinix_metal_port.bond0_ny]
+  depends_on = [equinix_metal_device_network_type.node2_network]
 }
 
-# Outputs for verification
-output "vlan1_id" {
-  value       = equinix_metal_vlan.vlan1.id
-  description = "VLAN ID for node1 in Silicon Valley"
+# Outputs for public and private IP verification
+output "node1_public_ip" {
+  value       = equinix_metal_device.node1.access_public_ipv4
+  description = "Public IP of node1 in Silicon Valley"
 }
 
-output "vlan2_id" {
-  value       = equinix_metal_vlan.vlan2.id
-  description = "VLAN ID for node2 in New York"
+output "node2_public_ip" {
+  value       = equinix_metal_device.node2.access_public_ipv4
+  description = "Public IP of node2 in New York"
 }
 
 output "node1_private_ip" {
@@ -95,12 +77,12 @@ output "node2_private_ip" {
   description = "Private IP of node2 in New York"
 }
 
-output "gateway_sv_ip" {
-  value       = equinix_metal_gateway.gateway_sv.private_ipv4_subnet_size
-  description = "IP range for Metal Gateway in Silicon Valley"
+output "vlan1_id" {
+  value       = equinix_metal_vlan.vlan1.id
+  description = "VLAN ID for node1 in Silicon Valley"
 }
 
-output "gateway_ny_ip" {
-  value       = equinix_metal_gateway.gateway_ny.private_ipv4_subnet_size
-  description = "IP range for Metal Gateway in New York"
+output "vlan2_id" {
+  value       = equinix_metal_vlan.vlan2.id
+  description = "VLAN ID for node2 in New York"
 }
